bbb9f1943183a9afcb46767854ec54a741478e96
Roxana Ion <ionroxana95@yahoo.com>
2020-05-02 16:05:53 +0200

    setup vuetify for typescript

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Inkline from '@inkline/inkline'
+import vuetify from './plugins/vuetify';
+import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Inkline);
+Vue.use(vuetify);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
